90a8c429888a7ea6f236216480e8d8008ab4f9fb
jahidhasan8 <jahidhasan1932@gmail.com>
2022-09-26 00:31:52 +0600

    added a cart component

---
+++ src/Cart/Cart.js
@@ -0,0 +1,14 @@
+
+
+import React from 'react';
+
+const Cart = ({cart}) => {
+    return (
+        <div>
+             <h3>Order summary</h3>
+     <p>selected items :{cart.length}</p>
+        </div>
+    );
+};
+
+export default Cart;
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import { useEffect } from 'react';
 import { useState } from 'react';
+import Cart from '../../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css';
 
@@ -33,8 +34,7 @@ const Shop = () => {
                 }
             </div>
             <div className="cart-container">
-                <h3>Order summary</h3>
-                <p>selected items :{cart.length}</p>
+               <Cart cart={cart}></Cart>
             </div>
         </div>
     );
