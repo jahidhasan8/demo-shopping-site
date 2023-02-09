32a5de6509ad7a31616daa40c8413c81e7de1ca7
jahidhasan8 <jahidhasan1932@gmail.com>
2023-02-09 14:10:06 +0600

    modified

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,83 +1,83 @@
 
-import React, { useContext } from 'react';
-import { Link } from 'react-router-dom';
-import logo from '../../images/Logo.svg'
-import { AuthContext } from '../contexts/UserContext';
-import './Header.css'
-const Header = () => {
-    const { user, logOut } = useContext(AuthContext)
-    return (
-        <nav className='header'>
-            <img className='logo' src={logo} alt="" />
-            <div className='nav-bar'>
-                <Link to="/">Shop</Link>
-                <Link to="/orders">Orders</Link>
-                <Link to="/inventory">Inventory</Link>
-                <Link to="/about">About</Link>
-                {
-                    user?.uid ?
-                        <button className='btn-logOut' onClick={logOut}>logOut</button>
-                        :
-                        <>
-                            <Link className='header-right' to="/login">Login</Link>
-                            <Link to="/signup">Signup</Link>
-                        </>
-
-                }
-                {/* <span>{user?.email}</span> */}
-            </div>
-        </nav>
-    );
-};
-
-export default Header;
-
-
-
 // import React, { useContext } from 'react';
-// import { Container, Nav, Navbar } from 'react-bootstrap';
-// // import logo from '../../images/Logo.svg'
 // import { Link } from 'react-router-dom';
+// import logo from '../../images/Logo.svg'
 // import { AuthContext } from '../contexts/UserContext';
-
+// import './Header.css'
 // const Header = () => {
 //     const { user, logOut } = useContext(AuthContext)
-
-//     // const handleLogout = () => {
-//     //     logOut()
-//     //         .then(() => { })
-//     //         .catch(error => console.error(error.message))
-//     // }
 //     return (
-//         <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-//             <Container>
-//                 <Navbar.Brand as={Link} to={"/"}>Maisha's World</Navbar.Brand>
-//                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-//                 <Navbar.Collapse id="responsive-navbar-nav">
-//                     <Nav className="me-auto bg-dark text-white">
-//                     <Nav.Link to="/">Shop</Nav.Link>
-//                 <Nav.Link to="/orders">Orders</Nav.Link>
-//                  <Nav.Link to="/inventory">Inventory</Nav.Link>
-//                  <Nav.Link to="/about">About</Nav.Link>
+//         <nav className='header'>
+//             <img className='logo' src={logo} alt="" />
+//             <div className='nav-bar '>
+//                 <Link to="/">Shop</Link>
+//                 <Link to="/orders">Orders</Link>
+//                 <Link to="/inventory">Inventory</Link>
+//                 <Link to="/about">About</Link>
+//                 {
+//                     user?.uid ?
+//                         <button className='btn-logOut' onClick={logOut}>logOut</button>
+//                         :
+//                         <>
+//                             <Link className='header-right' to="/login">Login</Link>
+//                             <Link to="/signup">Signup</Link>
+//                         </>
 
-//                     </Nav>
-//                     <Nav>
-
-//                         {
-//                             user?.uid ?
-//                                                     <button className='btn-logOut' onClick={logOut}>logOut</button>
-//                                                    :
-//                                                    <>
-//                                                        <Nav.Link className='header-right' to="/login">Login</Nav.Link>
-//                                                        <Nav.Link to="/signup">Signup</Nav.Link>
-//                                                     </>
-//                         }
-
-//                     </Nav>
-//                 </Navbar.Collapse>
-//             </Container>
-//         </Navbar>
+//                 }
+//                 {/* <span>{user?.email}</span> */}
+//             </div>
+//         </nav>
 //     );
 // };
 
 // export default Header;
+
+
+
+import React, { useContext } from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+// import logo from '../../images/Logo.svg'
+import { Link } from 'react-router-dom';
+import { AuthContext } from '../contexts/UserContext';
+
+const Header = () => {
+    const { user, logOut } = useContext(AuthContext)
+
+    // const handleLogout = () => {
+    //     logOut()
+    //         .then(() => { })
+    //         .catch(error => console.error(error.message))
+    // }
+    return (
+        <Navbar collapseOnSelect expand="lg" bg="danger" variant="dark">
+            <Container>
+                <Navbar.Brand as={Link} to={"/"}>Maisha's World</Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="me-auto bg-danger text-white">
+                    <Nav.Link as={Link} to="/">Shop</Nav.Link>
+                <Nav.Link as={Link} to="/orders">Orders</Nav.Link>
+                 <Nav.Link as={Link} to="/inventory">Inventory</Nav.Link>
+                 <Nav.Link as={Link} to="/about">About</Nav.Link>
+
+                    </Nav>
+                    <Nav>
+
+                        {
+                            user?.uid ?
+                                                    <button className='btn-logOut' onClick={logOut}>logOut</button>
+                                                   :
+                                                   <>
+                                                       <Nav.Link as={Link} className='header-right' to="/login">Login</Nav.Link>
+                                                       <Nav.Link as={Link} to="/signup">Signup</Nav.Link>
+                                                    </>
+                        }
+
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
+};
+
+export default Header;
--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,5 +1,5 @@
 
- 
+
 import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
@@ -8,23 +8,23 @@ import React from 'react';
 import './Product.css'
 
 
-const Product = ({product,handleAddToCart}) => {
-    const {img, name, seller, price, ratings}=product;
+const Product = ({ product, handleAddToCart }) => {
+    const { img, name, seller, price, ratings } = product;
     // const{handleAddToCart}=props;
     return (
         <div className='product'>
             <img src={img} alt="" />
-           <div className='product-info'>
-           <h3 className='product-name'>Name : {name}</h3>
-            <h3>Seller : {seller}</h3>
-            <h3>Price : ${price}</h3>
-            <h3>Ratings : {ratings}</h3>
-           </div>
-           <button onClick={()=>handleAddToCart(product)} className='btn-cart'>
-            <p className='btn-text'>Add To Cart</p>
-            <FontAwesomeIcon icon={faShoppingCart}></FontAwesomeIcon>
-            
-           </button>
+            <div className='product-info'>
+                <h3 className='product-name'>Name : {name}</h3>
+                <h3>Seller : {seller}</h3>
+                <h3>Price : ${price}</h3>
+                <h3>Ratings : {ratings}</h3>
+            </div>
+            <button onClick={() => handleAddToCart(product)} className='btn-cart'>
+                <p className='btn-text'>Add To Cart</p>
+                <FontAwesomeIcon icon={faShoppingCart}></FontAwesomeIcon>
+
+            </button>
         </div>
     );
 };
